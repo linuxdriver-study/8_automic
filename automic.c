63a6d8d530afb520b01c6c1dfe1a41b40e1117dd
wanglei <crazywl666666@gmail.com>
2021-09-03 21:02:13 +0800

    [feat]perfect driver.
    Signed-off-by: wanglei <crazywl666666@gmail.com>

--- automic.c
+++ automic.c
@@ -9,7 +9,7 @@
 #include <linux/gpio.h>
 #include <linux/of_gpio.h>
 
-#define DEVICE_CNT            1
+#define DEVICE_CNT              1
 #define DEVICE_NAME             "automic"
 
 struct automic_dev_struct {
@@ -17,6 +17,8 @@ struct automic_dev_struct {
         int major;
         int minor;
         struct cdev *cdev;
+        struct class *class;
+        struct device *device;
 };
 static struct automic_dev_struct automic_dev;
 
@@ -41,14 +43,43 @@ static int __init automic_init(void)
         }
         printk("major:%d minor:%d\n", automic_dev.major, automic_dev.minor);
 
-        cdev_init(automic_dev.cdev, );
+        cdev_init(automic_dev.cdev, &ops);
+        ret = cdev_add(automic_dev.cdev, automic_dev.devid, DEVICE_CNT);
+        if (ret < 0) {
+                printk("cdev add error!\n");
+                goto fail_cdev_add;
+        }
 
+        automic_dev.class = class_create(THIS_MODULE, DEVICE_NAME);
+        if (IS_ERR(automic_dev.class)) {
+                printk("class create error!\n");
+                goto fail_class_create;
+        }
+        automic_dev.device = device_create(automic_dev.class, NULL, 
+                                           automic_dev.devid, NULL, DEVICE_NAME);
+        if (IS_ERR(automic_dev.class)) {
+                printk("device create error!\n");
+                goto fail_device_create;
+        }
+
+        goto success;
+
+fail_device_create:
+        class_destroy(automic_dev.class);
+fail_class_create:
+        cdev_del(automic_dev.cdev);
+fail_cdev_add:
+        unregister_chrdev_region(automic_dev.devid, DEVICE_CNT);
 fail_region:
+success:
         return ret;
 }
 
 static void __exit automic_exit(void)
 {
+        device_destroy(automic_dev.class, automic_dev.devid);
+        class_destroy(automic_dev.class);
+        cdev_del(automic_dev.cdev);
         unregister_chrdev_region(automic_dev.devid, DEVICE_CNT);
 }
 
